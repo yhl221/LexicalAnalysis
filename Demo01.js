2ba8de94d7ae1a1f006ee9fa2f1a24e06b4e7529
yhl221 <1727538058@qq.com>
2016-10-12 09:44:35 +0800

    finish all the function

--- Demo01.js
+++ Demo01.js
@@ -1,30 +1,30 @@
 
-let  variable = [];
-let  number = [];
-let  retainPos;
- let symbolPos;
-function isBlank(char) {
+let  variable = [];//变量名表
+let  number = [];//常数表
+let  retainPos; //关键字种别码
+ let symbolPos; //界符种别码
+function isBlank(char) {//判断是否是空格
     if (char == 32) {
         return true;
     }
     return false;
 }
 
-function isLetter(char) {
+function isLetter(char) {//是否是字母
     if ((char >= 65 && char <= 90) || (char >= 97 && char <= 122)) {
         return true;
     }
     return false;
 }
 
-function isDigit(char) {
+function isDigit(char) {//判断是否是数字
     if (char >= 48 && char <= 57) {
         return true;
     }
     return false;
 }
 
-function isSymbol(symbol,strToken) {
+function isSymbol(symbol,strToken) {//判断是否是界符
     for(let i=0;i<symbol.length;i++){
         if(strToken===symbol[i]){
           symbolPos=36+i;
@@ -34,7 +34,7 @@ function isSymbol(symbol,strToken) {
     return false;
 }
 
-function getRetainWord(retainWord,strToken){
+function isRemainWord(retainWord, strToken){//判断是否是关键字
 
     for(let i=0;i<retainWord.length;i++){
         if(strToken===retainWord[i]){
@@ -46,34 +46,60 @@ function getRetainWord(retainWord,strToken){
     return false;
 }
 
-function type(number,variable,strToken) {
-    for(let i=0;i<number.length;i++){
+function isNumber(strToken) {//判断是否是常数
+    for(let i=0;i<strToken.length;i++){
+        if(isDigit(strToken[i].charCodeAt()))
+            return true;
+    }
+
+    return false;
+}
+
+function existNumber(number,strToken) {
+    //判断常数表中是否存在该常数，如果存在，则return；如果不存在，则将该常数插入常数表
+    for(let i=0;i<=number.length;i++){
         if(number[i]===strToken)
-            return 1;
+            return;
     }
 
-    for(let i=0;i<variable.length;i++){
+    number.push(strToken);
+}
+
+function isVariable(strToken) {
+    //判断是否为变量
+  for(let i=0;i<strToken.length;i++){
+      let unicode = strToken[i].charCodeAt();
+      if((isLetter(unicode)||isDigit(unicode)) && !isRemainWord(strToken))
+          return true;
+  }
+    return false;
+
+}
+
+function existVariable(variable,strToken) {
+    //判断变量表中是否存在该变量名，如果存在,则return;如果不存在，则将该变量名插入变量名表
+    for(let i=0;i<=variable.length;i++){
         if(variable[i]===strToken)
-            return 2;
+            return;
     }
 
-    return 0;
+    variable.push(strToken);
 }
 
 function main() {
     let strToken = "";    //存放构成单词符号的字符串
-
+    let string="";
+     let result=[];//用来存放分析结果的字符串
     const retainWord = ["main",
         "auto", "short", "int", "long", "float", "double", "char", "struct"
         , "union", "enum", "typedef", "const", "unsigned", "signed", "extern", "register"
         , "static", "volatile", "void", "if", "else", "switch", "case", "for"
         , "do", "while", "goto", "continue", "break", "default", "sizeof", "return"];
-    //变量名表和常数表在词法分析过程中建立
     const symbol=[ "=","+","-","*","/","++","--","+=","-=",
         "*=","/=","==","!=",">","<",">=","<=","(",
         ")","[","]","{","}",",",":",";"];
 
-    const inputString="int main ( ) { int a = 1 ; int b = 3 ; int sum = a + b ; }";
+    const inputString="int main ( ) { int a = 1 ; int b = 3 ; int sum = a + b ; } ";
     const stringArray = inputString.split("");
      for(let i=0;i<stringArray.length;i++){
          let unicode = stringArray[i].charCodeAt();
@@ -88,17 +114,30 @@ function main() {
              } else if(isSymbol(symbol,char)) {
                  strToken = strToken.concat(char);
              }
-         }else if(type(number,variable,strToken)===1){
-             console.log("('" + 34 + "','" + strToken + "')");
-         }else if(type(number,variable,strToken)===2){
-             console.log("('" + 33 + "','" + strToken + "')");
-         }else if(getRetainWord(retainWord,strToken)){
-             console.log("('" + retainPos + "','" +strToken+ "')");
+         }else if(isNumber(strToken)){
+             existNumber(number,strToken);
+             string = `(34,'${strToken}')`;//将输出结果以字符串
+             result.push(string);//将结果字符串存入数组
+             strToken="";
+         }else if(isVariable(strToken)){
+             existVariable(variable,strToken);
+             string = `(33,'${strToken}')`;
+             result.push(string);
+             strToken="";
+         }else if(isRemainWord(retainWord,strToken)){
+             string = `(${retainPos},'${strToken}')`;
+             result.push(string);
+             strToken="";
          }else if(isSymbol(symbol,strToken)){
-             console.log("('" +symbolPos + "','" +strToken+ "')");
-         }else{
+             string = `(${symbolPos},'${strToken}')`;
+             result.push(string);
              strToken="";
          }
+     }
+
+     for(let i=0;i<result.length;i++){
+         //输出结果
+         console.log(result[i]);
      }
 }
 
